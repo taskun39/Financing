551f20997d6ab5183164e21fe422ecd355601365
taskun39 <plus.plus.tasu@gmail.com>
2018-11-29 17:15:55 +0900

    First Commit

---
+++ Financing/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Financing.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
